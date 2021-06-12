7b95abb15f055d4780a19d1d9d93598ee723979e
Wandes Gomes <wandessilva@outlook.com>
2021-06-12 19:55:23 -0300

    implementando Feign

--- Loja/src/main/java/br/com/alura/microservice/loja/LojaApplication.java
+++ Loja/src/main/java/br/com/alura/microservice/loja/LojaApplication.java
@@ -3,11 +3,13 @@ package br.com.alura.microservice.loja;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableFeignClients
 public class LojaApplication {
 	
 	@Bean
--- Loja/src/main/java/br/com/alura/microservice/loja/service/CompraService.java
+++ Loja/src/main/java/br/com/alura/microservice/loja/service/CompraService.java
@@ -6,27 +6,34 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-
+import br.com.alura.microservice.loja.client.FornecedorClient;
 import br.com.alura.microservice.loja.controller.dto.CompraDTO;
 import br.com.alura.microservice.loja.dto.InfoFornecedorDTO;
 
 @Service
 public class CompraService {
 
-	@Value("${app.url.fornecedor}")
-	private String fornecedor;
+	//@Value("${app.url.fornecedor}")
+	//private String fornecedor;
+	
+	//@Autowired
+	//private RestTemplate client;
 	
 	@Autowired
-	private RestTemplate client;
+	private FornecedorClient fornecedorClient;
 	
 	public void realizaCompra(CompraDTO compra) {
-
-		//RestTemplate client = new RestTemplate();
+		
+		/** comunicação com RestTemplate
+		RestTemplate client = new RestTemplate();
 		
 		ResponseEntity<InfoFornecedorDTO> exchange = client.exchange(
 				fornecedor + compra.getEndereco().getEstado(), HttpMethod.GET, null, InfoFornecedorDTO.class);
-
-		System.out.println("->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>" + exchange.getBody().getEndereco());
+		**/
+		InfoFornecedorDTO info = fornecedorClient.getInfoPorEstado(compra.getEndereco().getEstado());
+		
+		//System.out.println(exchange.getBody().getEndereco());
+		System.out.println(info.getEndereco());
 
 	}
 
